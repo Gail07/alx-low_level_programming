91b6610e8db184599a16535532d147a70da489c1
Kacou Abigail <kacouabigail0@gmail.com>
2022-03-11 19:41:59 -0800

    All files of this project

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ * Return: (0)
+ */
+int main(void)
+{
+int k;
+
+for (k = 48; k <= 57; k++)
+{
+putchar(k);
+if (k < 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
