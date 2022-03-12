f1c9630198a7ee4d1c589318355544f59e50c666
Kacou Abigail <kacouabigail0@gmail.com>
2022-03-11 19:17:53 -0800

    All files of this project

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: (0)
+ */
+int main(void)
+{
+int k;
+
+for (k = 48; k <= 57; k++)
+{
+putchar(k);
+}
+putchar('\n');
+return (0);
+}
