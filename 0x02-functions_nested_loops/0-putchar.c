61b85cf7a376563c97a69106d24f140c60ca3637
Kacou Abigail <kacouabigail0@gmail.com>
2022-03-15 16:03:41 -0700

    File1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i;
+	char word[9] = "_putchar";
+	int i = 0;
 
-	for (i = 0; i < 9; i++)
+	while (i < 8)
 	{
-		_putchar(text[i]);
+		_putchar(word[i]);
+		i++;
 	}
 	_putchar('\n');
 	return (0);
