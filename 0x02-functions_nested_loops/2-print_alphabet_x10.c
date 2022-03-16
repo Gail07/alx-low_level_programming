1bb23ddc7ba52d143e623aefede01fd110079cec
Kacou Abigail <kacouabigail0@gmail.com>
2022-03-15 18:58:49 -0700

    File3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,16 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	char c;
+	int n, c;
 
-	for (i = 0; i < 10; ++i)
+	c = 0;
+	while (c < 10)
 	{
 		for (n = 'a'; n <= 'z'; n++)
 		{
-		_putchar(c);
+		_putchar(n);
 		}
+	c++;
 	_putchar('\n');
 	}
 }
