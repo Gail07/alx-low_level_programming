470a12acb3d2323176f3caf29f456cd2d5dd6b1b
Kacou Abigail <kacouabigail0@gmail.com>
2022-04-25 17:19:32 -0700

    Fixed task

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * sum_listint - returns the sum of all the data (n) of a listint_t linked list.
+ * sum_listint - returns the sum of all the data (n) of a listint_t linked list
  * @head: pointer to the list.
  *
  * Return: all the data (n), 0 if the list is empty
@@ -11,7 +11,7 @@ int sum_listint(listint_t *head)
 {
 	int sum = 0;
 
-	while(head != NULL)
+	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
