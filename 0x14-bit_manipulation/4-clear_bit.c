bcdb5cbcc720bc953024af2d51d29305f25b06dd
Kacou Abigail <kacouabigail0@gmail.com>
2022-04-28 16:51:01 -0700

    Task done

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long int set;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
-			return (-1);
+		return (-1);
 	set = ~(1 << index);
 	*n = *n & set;
 	return (1);
